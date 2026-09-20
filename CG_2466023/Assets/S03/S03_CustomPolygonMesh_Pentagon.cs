4a1169b3dd34711fa36fd5f9469414b62ab1da70
seola522 <seola522@ewha.ac.kr>
2026-09-20 18:27:28 +0900

    S02-commit 2차

--- CG_2466023/Assets/S03/S03_CustomPolygonMesh_Pentagon.cs
+++ CG_2466023/Assets/S03/S03_CustomPolygonMesh_Pentagon.cs
@@ -1,29 +1,35 @@
-﻿  using UnityEngine;
+﻿using UnityEngine;
 
-  [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-  public class S03_CustomPolygonMesh_Pentagon : MonoBehaviour
-  {
-      void Start()
-      {
-          // TODO 1: 정점 좌표 정의(5개 이상)
-          Vector3[] vertices = new Vector3[]
-          {
-              // 예: new Vector3(0f, 1f, 0f),
-              
-          };
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_CustomPolygonMesh_Pentagon : MonoBehaviour
+{
+    void Start()
+    {
+        
+        // TODO 1: 정점 좌표 정의(5개 이상)
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(-1f,  2f, 0f), // 0: 좌상단
+            new Vector3(-2f,  0f, 0f), // 1: 좌측
+            new Vector3(-1f, -2f, 0f), // 2: 좌하단
+            new Vector3( 1f, -2f, 0f), // 3: 우하단
+            new Vector3( 2f,  0f, 0f), // 4: 우측
+            new Vector3( 1f,  2f, 0f), // 5: 우상단
 
-          // TODO 2: 삼각형 인덱스 정의
-          int[] triangles = new int[]
-          {
-              
-          };
+        };
 
-          Mesh mesh = new Mesh();
-          mesh.vertices = vertices;
-          mesh.triangles = triangles;
-          mesh.RecalculateNormals();
+        // TODO 2: 삼각형 인덱스 정의
+        int[] triangles = new int[]
+        {
 
-          GetComponent<MeshFilter>().mesh = mesh;
-          GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
-      }
-  }
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
