965f850e1cf2de5e36b710df22820a5f12fac7a4
seola522 <seola522@ewha.ac.kr>
2026-09-20 19:32:02 +0900

    S04-2차 commit

--- CG_2466023/Assets/S04/S04_Diamond.cs
+++ CG_2466023/Assets/S04/S04_Diamond.cs
@@ -8,6 +8,12 @@ public class S04_Diamond : MonoBehaviour
         // TODO 1: 정점 6개 정의 (위/아래 꼭짓점 + 가운데 고리 4개)
         Vector3[] vertices = new Vector3[]
         {
+            new Vector3( 0f,  1f,  0f), // 0
+            new Vector3( 0f, -1f,  0f), // 1
+            new Vector3( 0f,  0f,  1f), // 2
+            new Vector3( 1f,  0f,  0f), // 3
+            new Vector3( 0f,  0f, -1f), // 4
+            new Vector3(-1f,  0f,  0f), // 5
         };
 
         // TODO 2: 삼각형 8개(위 4개 + 아래 4개) winding order 정의
