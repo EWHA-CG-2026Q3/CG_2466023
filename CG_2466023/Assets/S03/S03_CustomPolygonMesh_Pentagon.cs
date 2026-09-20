432438d3c9603b8f2bf5aac8f6b2513552925872
seola522 <seola522@ewha.ac.kr>
2026-09-20 18:09:28 +0900

    S03-1차 커밋
    1차 커밋

---
+++ CG_2466023/Assets/S03/S03_CustomPolygonMesh_Pentagon.cs
@@ -0,0 +1,29 @@
+﻿  using UnityEngine;
+
+  [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+  public class S03_CustomPolygonMesh_Pentagon : MonoBehaviour
+  {
+      void Start()
+      {
+          // TODO 1: 정점 좌표 정의(5개 이상)
+          Vector3[] vertices = new Vector3[]
+          {
+              // 예: new Vector3(0f, 1f, 0f),
+              
+          };
+
+          // TODO 2: 삼각형 인덱스 정의
+          int[] triangles = new int[]
+          {
+              
+          };
+
+          Mesh mesh = new Mesh();
+          mesh.vertices = vertices;
+          mesh.triangles = triangles;
+          mesh.RecalculateNormals();
+
+          GetComponent<MeshFilter>().mesh = mesh;
+          GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+      }
+  }
