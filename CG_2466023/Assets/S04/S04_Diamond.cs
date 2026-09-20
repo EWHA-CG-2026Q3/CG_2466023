5153a592a993bd6f449fc040285f29567b4040ac
seola522 <seola522@ewha.ac.kr>
2026-09-20 19:29:31 +0900

    S04_1차 commit

---
+++ CG_2466023/Assets/S04/S04_Diamond.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S04_Diamond : MonoBehaviour
+{
+    void Start()
+    {
+        // TODO 1: 정점 6개 정의 (위/아래 꼭짓점 + 가운데 고리 4개)
+        Vector3[] vertices = new Vector3[]
+        {
+        };
+
+        // TODO 2: 삼각형 8개(위 4개 + 아래 4개) winding order 정의
+        int[] triangles = new int[]
+        {
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
